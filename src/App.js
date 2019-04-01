2b54aa95df199a5bb3851770d181bcf63d74ecfb
Jon Gilbert <jonglbrt@gmail.com>
2019-03-31 22:56:24 -0400

    Added functions for Controls and showing content

--- src/App.js
+++ src/App.js
@@ -5,11 +5,55 @@ import BottomControls from './components/BottomControls'
 import './css/style.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      questStatus: true,
+      completedStatus: false,
+      descriptionStatus: true,
+      rewardStatus: false
+    }
+  }
+
+  showCompleted = () => {
+    console.log('works')
+    this.setState({ questStatus: false, completedStatus: true })
+  }
+
+  showRewards = () => {
+    console.log('alsoworks')
+    this.setState({ rewardStatus: true, descriptionStatus: false })
+  }
+
+  showQuests = () => {
+    console.log('thisworks')
+    this.setState({ questStatus: true, completedStatus: false })
+  }
+
+  showDescription = () => {
+    console.log('test')
+    this.setState({ rewardStatus: false, descriptionStatus: true })
+  }
+
   render() {
     return (
       <div className='container'>
-        <TopControls />
-        <MainContent />
+        <TopControls
+          showCompleted={this.showCompleted}
+          showRewards={this.showRewards}
+          showQuests={this.showQuests}
+          showDescription={this.showDescription}
+          questStatus={this.state.questStatus}
+          completedStatus={this.state.completedStatus}
+          descriptionStatus={this.state.descriptionStatus}
+          rewardStatus={this.state.rewardStatus}
+        />
+        <MainContent
+          questStatus={this.state.questStatus}
+          completedStatus={this.state.completedStatus}
+          descriptionStatus={this.state.descriptionStatus}
+          rewardStatus={this.state.rewardStatus}
+        />
         <BottomControls />
       </div>
     )
--- src/components/TopControls.js
+++ src/components/TopControls.js
@@ -1,12 +1,67 @@
 import React, { Component } from 'react'
 
 class TopControls extends Component {
+  handleLeftControlCompleted = () => {
+    if (this.props.questStatus) {
+      return 'show-completed'
+    } else {
+      return 'no-display'
+    }
+  }
+
+  handleLeftControlQuests = () => {
+    if (this.props.completedStatus) {
+      return 'show-quests'
+    } else {
+      return 'no-display'
+    }
+  }
+
+  handleRightControlRewards = () => {
+    if (this.props.descriptionStatus) {
+      return 'show-rewards'
+    } else {
+      return 'no-display'
+    }
+  }
+
+  handleRightControlDescription = () => {
+    if (this.props.rewardStatus) {
+      return 'show-description'
+    } else {
+      return 'no-display'
+    }
+  }
+
   render() {
     return (
       <div className='control-container-top'>
         <h3 className='control-top-header'>Quest Log</h3>
-        <span className='show-completed'>Show Completed</span>
-        <span className='show-rewards'>Show Rewards</span>
+        <span
+          className={this.handleLeftControlCompleted()}
+          onClick={this.props.showCompleted}
+        >
+          Show Completed
+        </span>
+        <span
+          className={this.handleLeftControlQuests()}
+          onClick={this.props.showQuests}
+        >
+          Show Quests
+        </span>
+        <span
+          className={this.handleRightControlRewards()}
+          onClick={this.props.showRewards}
+        >
+          Show Rewards
+        </span>
+
+        <span
+          className={this.handleRightControlDescription()}
+          onClick={this.props.showDescription}
+        >
+          Show Description
+        </span>
       </div>
     )
   }
